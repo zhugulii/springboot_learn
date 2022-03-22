cb7154bdd2a31e4bb33071ec2f9b4a3511fd8dd1
zhuguli <18009244708@163.com>
2022-03-22 15:35:10 +0800

    init springboot learn demo

---
+++ src/main/java/com/guli/springboot_learn/SpringbootLearnApplication.java
@@ -0,0 +1,13 @@
+package com.guli.springboot_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLearnApplication.class, args);
+    }
+
+}
